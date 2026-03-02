12c0ff5f20553e483592f053fcfd13ce1cf0d3a3
atharva9trix <atharva.gornal@navaantrix.com>
2026-03-02 11:23:05 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    print(data)
+    return {"status": "received"}
