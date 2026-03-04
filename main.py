8885303c0679457488e298f6b55c339f945aeae2
onkarp9trix <onkar.panchakshari@navaantrix.com>
2026-03-04 13:20:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
+# from fastapi import FastAPI, Request
+# import requests
+
+# app = FastAPI()
+
+# EVOLUTION_URL = "https://whatsapp-1-evolution-api.n0r6ff.easypanel.host"
+# INSTANCE = "whatsappbot"
+# API_KEY = "5D0A84B47ED9-42A1-B2D2-DF565E539746"
+
+
+# @app.get("/")
+# def home():
+#     return {"status": "running"}
+
+
+# @app.post("/webhook")
+# async def webhook(request: Request):
+
+#     data = await request.json()
+#     print(data)
+
+#     try:
+#         number = data["data"]["key"]["remoteJid"].replace("@s.whatsapp.net", "")
+
+#         message = data["data"]["message"].get("conversation") or \
+#                   data["data"]["message"]["extendedTextMessage"]["text"]
+
+#         send_message(number, "Hello 👋 I am your bot")
+
+#     except Exception as e:
+#         print("Error:", e)
+
+#     return {"status": "received"}
+
+
+# def send_message(number, text):
+
+#     url = f"{EVOLUTION_URL}/message/sendText/{INSTANCE}"
+
+#     payload = {
+#         "number": number,
+#         "text": text
+#     }
+
+#     headers = {
+#         "apikey": API_KEY
+#     }
+
+#     requests.post(url, json=payload, headers=headers)
+
 from fastapi import FastAPI, Request
 import requests
 
 app = FastAPI()
 
-EVOLUTION_URL = "https://whatsapp-1-evolution-api.n0r6ff.easypanel.host"
-INSTANCE = "whatsappbot"
-API_KEY = "5D0A84B47ED9-42A1-B2D2-DF565E539746"
-
-
-@app.get("/")
-def home():
-    return {"status": "running"}
-
-
-@app.post("/webhook")
+@app.api_route("/webhook", methods=["GET","POST"])
 async def webhook(request: Request):
 
+    if request.method == "GET":
+        return {"status": "webhook alive"}
+
     data = await request.json()
-    print(data)
 
-    try:
-        number = data["data"]["key"]["remoteJid"].replace("@s.whatsapp.net", "")
-
-        message = data["data"]["message"].get("conversation") or \
-                  data["data"]["message"]["extendedTextMessage"]["text"]
-
-        send_message(number, "Hello 👋 I am your bot")
-
-    except Exception as e:
-        print("Error:", e)
+    print("WEBHOOK RECEIVED:", data)
 
     return {"status": "received"}
-
-
-def send_message(number, text):
-
-    url = f"{EVOLUTION_URL}/message/sendText/{INSTANCE}"
-
-    payload = {
-        "number": number,
-        "text": text
-    }
-
-    headers = {
-        "apikey": API_KEY
-    }
-
-    requests.post(url, json=payload, headers=headers)
