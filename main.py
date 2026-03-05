32c0095d24a29925730aa24f49bbef6cb1f3b7e9
onkarp9trix <onkar.panchakshari@navaantrix.com>
2026-03-05 12:20:33 +0530

    Change EVOLUTION_URL to use HTTP and port 8080

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 
 app = FastAPI()
 
-EVOLUTION_URL = "https://whatsapp-1-evolution-api.n0r6ff.easypanel.host"
+EVOLUTION_URL = "http://whatsapp-1-evolution-api:8080"
 INSTANCE = "whatsappbot"
 API_KEY = "4EB3C96EBCB4-4CE2-864C-0FF043FB41EC"
 
